00f0a6e3d41e4cc037016e0120d0fadad8d4055e
marsianca <xmarsiancax@mail.ru>
2022-02-18 10:22:40 +0300

    added Route to registration and login

--- src/components/content/main-content.tsx
+++ src/components/content/main-content.tsx
@@ -1,11 +1,17 @@
 import Login from "../login/login"
+import {Route, Routes} from "react-router-dom";
+import StartContent from "./start-content/start-content";
+import Registration from "../registration/registration";
+
 const MainContent = () => {
     return (
         <div>
-            {/*<StartContent/>*/}
-            {/*<TasksContent/>*/}
-            {/*<Registration/>*/}
-            <Login/>
+            <Routes>
+                <Route path="/" element={<StartContent/>}/>
+                {/*<TasksContent/>*/}
+                <Route path="/registration" element={<Registration/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
         </div>
     )
 }
